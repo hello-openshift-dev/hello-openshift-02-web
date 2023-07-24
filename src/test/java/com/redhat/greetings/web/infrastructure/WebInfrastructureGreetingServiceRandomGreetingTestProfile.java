94d3a06645a2883e12728f57b13ec8acc36c2ae0
jeremyrdavis <jeremyrdavis@mac.com>
2023-07-24 05:35:41 -0400

    adding greetings

---
+++ src/test/java/com/redhat/greetings/web/infrastructure/WebInfrastructureGreetingServiceRandomGreetingTestProfile.java
@@ -0,0 +1,10 @@
+package com.redhat.greetings.web.infrastructure;
+
+import io.quarkus.test.junit.QuarkusTestProfile;
+
+/**
+ * Empty profile used to start a new instance of Quarkus so that the @PostConstruct method in GreetingService is injected
+ * with the correct Mock in GreetingServiceTest
+ */
+public class WebInfrastructureGreetingServiceRandomGreetingTestProfile  implements QuarkusTestProfile {
+}
